758d2a21a0228ea9e21eed8b0f4652cc7db4c4f5
KilloconQ <fjc1997v@gmail.com>
2024-06-22 12:53:21 -0600

    commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,18 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Commandalias cl clear
 end
 
-#Vim
-alias v="nvim"
-
-#Clear
-alias cl="clear"
-
-#Git
-alias ga="git add ."
-alias gc="git commit -m"
-alias gs="git status"
-alias gst="git stash"
-alias gstp="git stash pop"
-alias gsw="git switch"
-alias gswc="git switch -c"
-alias gb="git branch"
-alias gbd="git branch -D"
+alias ga 'git add .'
+alias gb 'git branch'
+alias gbd 'git branch -D'
+alias gc 'git commit -m'
+alias gpull 'git pull origin'
+alias gpush 'git push origin'
+alias gs 'git status'
+alias gst 'git stash'
+alias gstp 'git stash pop'
+alias gsw 'git switch'
+alias gswc 'git switch -c'
+alias v nvim
 
 set -gx PROJECT_PATHS ~/workspace ~/src
