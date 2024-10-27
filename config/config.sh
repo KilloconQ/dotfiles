55298dadfecec9add9a48bf7f134f504533dac71
KilloconQ <fjc1997v@gmail.com>
2024-10-27 14:14:09 -0600

    remove nix

--- config/config.sh
+++ config/config.sh
@@ -24,7 +24,7 @@ echo "Actualizando Homebrew..."
 brew update && brew upgrade
 
 echo "Instalando herramientas de línea de comandos..."
-brew install git gh wget fish neovim fzf zellij bat lsd
+brew install git gh wget fish neovim fzf zellij bat lsd fnm deno
 
 echo "Configurando fish..."
 
@@ -43,28 +43,41 @@ if ! grep -q "$(which fish)" /etc/shells; then
 fi
 chsh -s "$(which fish)"
 
-# Fuente la configuración de fish
 echo "Cargando configuración de fish..."
 fish -c 'source ~/dotfiles/fish/config.fish'
 
-echo "Instalando Node.js y npm..."
-LTS_VERSION=$(curl -sL https://nodejs.org/en/ | grep -oP '(?<=LTS: Node.js )\d+\.\d+\.\d+' | head -n 1)
-LTS_MAJOR_VERSION=$(echo "$LTS_VERSION" | cut -d. -f1)
-
-if brew search "node@$LTS_MAJOR_VERSION" | grep -q "node@$LTS_MAJOR_VERSION"; then
-  brew install "node@$LTS_MAJOR_VERSION"
-  fish_user_paths="/usr/local/opt/node@$LTS_MAJOR_VERSION/bin"
-  set -U fish_user_paths "$fish_user_paths" $fish_user_paths
+echo "Verificando instalación de Oh My Fish..."
+if fish -c 'type omf' &>/dev/null; then
+  echo "Oh My Fish ya está instalado. Omitiendo instalación."
 else
-  echo "No se encontró la fórmula para node@$LTS_MAJOR_VERSION en Homebrew"
+  echo "Instalando Oh My Fish..."
+  echo | curl -L https://get.oh-my.fish | fish
+fi
+
+echo "Configurando fnm en fish..."
+fish -c 'set -U fish_user_paths /usr/local/opt/fnm/bin $fish_user_paths'
+fish -c 'fnm env | source'
+
+echo "Gestionando instalación de Node.js LTS..."
+LTS_VERSION=$(fnm ls-remote | grep -E 'v[0-9]+\.[0-9]+\.[0-9]+.*LTS' | tail -n1 | awk '{print $1}')
+CURRENT_VERSION=$(fish -c 'node -v' 2>/dev/null || echo "")
+
+if [ "$CURRENT_VERSION" != "$LTS_VERSION" ]; then
+  echo "Instalando o actualizando Node.js a la versión LTS $LTS_VERSION..."
+  fish -c "fnm install $LTS_VERSION && fnm use $LTS_VERSION && fnm alias default $LTS_VERSION"
+else
+  echo "Node.js ya está actualizado a la versión LTS $LTS_VERSION."
 fi
 
 fish -c 'node -v'
 npm install -g yarn typescript
 
+echo "Instalando Deno versión 2..."
+brew install deno
+fish -c 'deno upgrade --version 2.0.0'
+
 echo "Creando enlaces simbólicos..."
 
-# Función para crear enlaces simbólicos correctamente
 create_symlink() {
   local target=$1
   local link=$2
@@ -77,16 +90,12 @@ create_symlink() {
   ln -sfn "$target" "$link"
 }
 
-# Crear enlaces simbólicos sin duplicar directorios
 create_symlink ~/dotfiles/nvim ~/.config/nvim
 create_symlink ~/dotfiles/zellij ~/.config/zellij
 create_symlink ~/dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
 create_symlink ~/dotfiles/omf ~/.config/omf
 create_symlink ~/dotfiles/config ~/.config/config
 create_symlink ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-
-echo "Instalando Oh My Fish..."
-curl -L https://get.oh-my.fish | fish
 
 echo "Instalando bun..."
 curl -fsSL https://bun.sh/install | bash
