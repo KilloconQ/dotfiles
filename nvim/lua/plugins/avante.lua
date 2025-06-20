6ef2060f8260d1a54e683d53a3dd99d1e63550a1
fcorralescarfaxmx <fcorrales@carfaxmx.com.mx>
2025-06-20 09:24:57 -0600

    imp: add avante to nvim

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -7,18 +7,7 @@ return {
     -- for example
     provider = "copilot",
     providers = {
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4.1", -- your desired model (or use claude-4-sonnet, etc.)
-        extra_request_body = {
-          timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-          temperature = 0.75,
-          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-        },
-      },
-      claude = {
-        endpoint = "https://api.anthropic.com/",
+      copilot = {
         model = "claude-sonnet-4",
       },
     },
