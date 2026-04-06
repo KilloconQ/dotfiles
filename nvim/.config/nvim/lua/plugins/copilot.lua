246cd771bdcb23f78650efb38d24c42bff4457fc
Killoconq <fjc1997v@gmail.com>
2026-04-06 08:06:56 -0600

    copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,20 +1,29 @@
 return {
   "zbirenbaum/copilot.lua",
   optional = true,
-  opts = function()
-    require("copilot.api").status = require("copilot.status")
-    require("copilot.api").filetypes = {
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-    }
-  end,
+  opts = {
+    filetypes = {
+      yaml = false,
+      markdown = false,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = false,
+    },
+    server_opts_overrides = {
+      root_dir = function(filename)
+        if not filename or filename == "" or vim.startswith(filename, "oil://") then
+          return vim.fn.getcwd()
+        end
+        local ok, util = pcall(require, "lspconfig.util")
+        if ok then
+          return util.root_pattern(".git", ".hg")(filename) or vim.fn.getcwd()
+        end
+        return vim.fn.getcwd()
+      end,
+    },
+  },
 }
