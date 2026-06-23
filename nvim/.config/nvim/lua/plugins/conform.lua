0e0070ad47b4a6164c34f580b3d8d2d46761e59b
Killoconq <fjc1997v@gmail.com>
2026-06-23 13:19:33 -0600

    mhos cambios

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,8 +1,19 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      astro = { "prettier" },
-    },
-  },
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    -- oxfmt owns JS/TS/JSON; assignment replaces prettier/biome instead of stacking
+    for _, ft in ipairs({
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "json",
+      "jsonc",
+    }) do
+      opts.formatters_by_ft[ft] = { "biome", "prettier", "oxfmt", stop_after_first = true }
+    end
+
+    opts.formatters_by_ft.astro = { "prettier" }
+  end,
 }
--- nvim/.config/nvim/lua/plugins/formatting.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      astro = { "prettier" }, -- reemplaza con esto para forzar solo prettier
-    },
-  },
-}
